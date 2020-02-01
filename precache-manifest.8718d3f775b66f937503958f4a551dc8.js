261f5ae6271be9b14a54a83ba3316e0f3e038311
Calin Cara <calincara@gmail.coma>
2020-02-01 12:46:41 +0200

    Updates

---
+++ precache-manifest.8718d3f775b66f937503958f4a551dc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb3cdb156299dd844b6cb4ea3125967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46eee2be3d435d59ddd9",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a39af12315a756a5030a",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "46eee2be3d435d59ddd9",
+    "url": "/static/js/2.a9f7297b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a9f7297b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39af12315a756a5030a",
+    "url": "/static/js/main.9707a566.chunk.js"
+  },
+  {
+    "revision": "1aed9091a153684323bb",
+    "url": "/static/js/runtime-main.2e65fac1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
